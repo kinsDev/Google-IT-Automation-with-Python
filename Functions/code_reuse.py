425d443acdcc424f82b5452ae9d9b493beea242f
kinsDev <kinsleygitonga@gmail.com>
2023-12-18 11:58:55 +0300

    code reuse using functions in python

---
+++ Functions/code_reuse.py
@@ -0,0 +1,18 @@
+# Traditionally we would achieve a solution the worng way, here is an example
+name = "Kay"
+number = len(name) * 9
+
+print("Hello " + name + ". Your lucky number is " + str(number))
+
+name = "Cameron"
+number = len(name) * 9
+
+print("Hello " + name + ". Your lucky number is " + str(number))
+
+# But we could use functions to achieve the solution in an easier way
+def lucky_number(name):
+    number = len(name) * 9
+    print("Hello " + name + ". Your lucky number is " + str(number))
+    
+lucky_number("Kay")
+lucky_number("Cameron")
