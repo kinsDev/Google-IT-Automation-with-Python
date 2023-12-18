00047f5d18cc4f8fca61492b2bff871147b1ec6d
kinsDev <kinsleygitonga@gmail.com>
2023-12-18 11:27:24 +0300

    Expression and Variables

---
+++ Expression_Variables/skill3.py
@@ -0,0 +1,5 @@
+print("5 * 3 = " + str(5*3))
+#
+# To avoid a type error between the string and the integer within the
+# print() function, you can make an explicit data type conversion by
+# using the str() function to convert the integer to a string.
