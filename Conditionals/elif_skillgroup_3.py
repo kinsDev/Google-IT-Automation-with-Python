2af5586cabf63a23ef3c90683f27d3cef2c02ed7
kinsDev <kinsleygitonga@gmail.com>
2024-03-05 14:01:17 +0300

    elif usage

---
+++ Conditionals/elif_skillgroup_3.py
@@ -0,0 +1,9 @@
+number = 25
+if number <= 5:
+    print("The number is 5 or smaller.")
+elif number == 33:
+    print("The number is 33.")
+elif number < 32 and number >= 6:
+    print("The number is less than 32 and greater than 6.")
+else:
+    print("The number is " + str(number))
