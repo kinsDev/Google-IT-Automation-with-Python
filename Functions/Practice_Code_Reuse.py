cdd61c7d8499be2bb044b2057981bc391dfdf761
kinsDev <kinsleygitonga@gmail.com>
2024-03-05 12:08:47 +0300

    Lets do this mate!

---
+++ Functions/Practice_Code_Reuse.py
@@ -0,0 +1,11 @@
+def calculated(d):
+    q = 3.14
+    z = q * (d ** 2)
+    print(z)
+calculated(5)
+
+def circle_area(radius):
+    pi = 3.14
+    area = pi * (radius **2)
+    print(area)
+circle_area(5)
