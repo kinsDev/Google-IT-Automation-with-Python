cbbf8ac06dd2ab2478af66fd341d2b28a1f96a49
kinsDev <kinsleygitonga@gmail.com>
2024-03-07 11:13:31 +0300

    initializing variables

---
+++ While Loops/initializing_variables.py
@@ -0,0 +1,10 @@
+#while my_variable < 10:
+#    print("Hello")
+#    my_variable += 1
+# This code will cause a name error
+# This is because we haven't defined a variable yet
+
+my_variable = 5
+while my_variable < 10:
+    print("Hello")
+    my_variable += 1
