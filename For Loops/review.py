6aef292cc9699c6f9a1663b418b8e1ec48958b03
kinsDev <kinsleygitonga@gmail.com>
2024-03-07 13:43:12 +0300

    woow, so far so good

--- For Loops/review.py
+++ For Loops/review.py
@@ -12,3 +12,13 @@ for value in values:
     sum += value
     length += 1
 print("Total sum: " + str(sum) + " -Average: " + str(sum/length))
+
+product = 1
+for n in range(1, 10):
+    product = product * n
+print(product)
+
+def to_celcius(x):
+    return (x - 32)*5/9
+for x in range(0, 101, 10):
+    print(x, to_celcius(x))
