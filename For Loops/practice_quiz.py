b421ef39c690ad922a1dac91bf62a887fef7eefa
kinsDev <kinsleygitonga@gmail.com>
2024-03-15 13:59:58 +0300

    so far so good

--- For Loops/practice_quiz.py
+++ For Loops/practice_quiz.py
@@ -12,3 +12,20 @@ print("\n")
 
 for y in range(6, 22, 3):
   print(y * 2)
+print("\n")
+
+for n in range(0,18+1,2):
+  print(n*2)
+print("\n")
+
+# Question 4
+# Fill in the blanks so that the for loop will print the first 10 cube numbers (x**3) in a range that starts with x=1 and ends with x=10.
+for x in range(1, 11):
+  print(x ** 3)
+print("\n")
+
+# Write a for loop with a three parameter range() function that prints the multiples of 7 between 0 and 100. Print one multiple per line and avoid printing any numbers that aren't multiples of 7. Remember that 0 is also a multiple of 7.
+for x in range(0, 101, 7):
+    print(x)
+print("\n")
+
