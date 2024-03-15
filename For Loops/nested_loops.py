ec08fba60ccf605a75fda761f7714f3a0e833fe8
kinsDev <kinsleygitonga@gmail.com>
2024-03-15 10:22:23 +0300

    nested loops test

--- For Loops/nested_loops.py
+++ For Loops/nested_loops.py
@@ -1,5 +1,5 @@
 for left in range(7):
     for right in range(left, 7):
-        print("[" + str(left) + "|" + str(right) + "]")
+        print("[" + str(left) + "|" + str(right) + "]", end = " ")
     print()
 #%%
