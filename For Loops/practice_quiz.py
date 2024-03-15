1e3a73f1a8a8674224c4fad707d3f5cbf83e377d
kinsDev <kinsleygitonga@gmail.com>
2024-03-15 13:55:09 +0300

    practicing question 1

---
+++ For Loops/practice_quiz.py
@@ -0,0 +1,14 @@
+for n in range(6,18+1,3):
+  print(n*2)
+print("\n")
+
+for i in range(6, 19, 3):
+  print(i * 2)
+print("\n")
+
+for z in range(6, 20, 3):
+  print(z * 2)
+print("\n")
+
+for y in range(6, 22, 3):
+  print(y * 2)
