f09d2411fd0792cb8b5aae5050157f8e8a297eb0
kinsDev <kinsleygitonga@gmail.com>
2024-03-15 11:41:46 +0300

    slicing strings

---
+++ For Loops/slicing_joining.py
@@ -0,0 +1,9 @@
+string1 = "Greetings, Earthlings"
+print(string1[0])   # Prints “G”
+print(string1[4:8]) # Prints “ting”
+print(string1[11:]) # Prints “Earthlings”
+print(string1[:5])  # Prints “Greet”
+
+print(string1[-10:]) # Prints “Earthlings” again - python counts from end of string if it is negative
+
+# How to combine slicing and joining strings
