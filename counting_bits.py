044ef1d3c5f3731c8f747c40521e897c883973ff
kinsDev <kinsleygitonga@gmail.com>
2024-03-27 08:08:33 +0300

    all good now

--- counting_bits.py
+++ counting_bits.py
@@ -13,11 +13,8 @@ print(countBits(int(num)))
 """
 1. function countbits calls parameter num which is the user's input
 2. we define a counter to start counting the num at index 0
-3. if the num is greater than or equal to 1, and the len of the counter is less than num, we increment the counter by 1
-4. I don't know hoe to explain or read this code:
-counter = counter + [i + 1 for i in counter]
-        return counter[:num+1]
-    else:
-        return 0
--- so help me out on explaining from there!
+3. if the num is greater than or equal to 1, and the len of the counter is less than num
+4. We iterate over each element in the counter list and adding 1 to it hence doubling the values in the counter list
+5. we append the newly created list(double values) to the counter list  essentially doubling the size of the counter list
+6. we return all the number in the counter list inclusive of the last number in the counter list 
 """
